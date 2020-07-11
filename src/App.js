a7db9258cfa512dfdaa8473cac3c58b7980847cb
ntaroooo <45555581+ntaroooo@users.noreply.github.com>
2020-07-11 18:16:26 +0900

    second

--- src/App.js
+++ src/App.js
@@ -1,26 +1,156 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {BrowserRouter, Route, Link, Router} from 'react-router-dom';
+import './App.css';
+import {Battle} from './component/Battle';
+
+
+
+class App extends Component {
+  render(){   
+    return (
+      <div className="App">
+      
+        <Left />
+        <Title />
+      
+      
+      </div>
+    );
+  }
 }
+
+/*const App = () => (
+  <BrowserRouter >
+    <div>
+      <switch>
+
+
+      <Route exact path="/" component={Home} />
+      <Route  path="/battle" component={Battle} />
+      </switch>
+    </div>
+  </BrowserRouter>
+)*/
+
+const Home = () => (
+  
+  <div className="App">
+      
+        <Left />
+        <Title />
+      
+      
+  </div>
+
+
+)
+/*const Left = () => {
+  return(
+    <div className="left">
+      <div className="image">
+        <Component1 />
+      </div>
+      
+    </div>
+  )
+}*/
+
+const Left = () => {
+  return(
+    <div className="left">
+      
+     
+      
+    </div>
+  )
+}
+
+const Title = () => {
+  return(
+    <div className="title1">
+      <h1>Anime resercher</h1>
+      <Search />
+      <Tagu />
+      
+      
+    </div>
+  )
+}
+
+const Tagu = () => {
+  let items = [
+    {"name": "Battle"},
+    {"name": "Romantic"},
+    {"name": "School Life"},
+    {"name": "Robot"},
+    {"name": "This term"},
+    {"name": "My favorite"}
+  ]
+      /*return(
+        <div className="tagu">
+         
+          <BrowserRouter>
+          <div className="list">
+            <Link to="/battle">Battle</Link>
+            <Route  path="/battle" component={Battle} />
+            
+            
+            
+          </div>
+          </BrowserRouter>
+          
+          <div className="list">
+            Romantic
+          </div>
+          <div className="list">
+            School Life
+          </div>
+          <div className="list">
+            Robot
+          </div>
+          <div className="list">
+            This term
+          </div>
+          <div className="list">
+            My favorite
+          </div>
+         
+        </div>
+      )*/
+    return(
+      <div className="tagu">
+        <button>Battle</button>
+      </div>
+    )
+ /* return(
+    <div className="tagu">
+      <BrowserRouter>
+        <ul>
+          <li><Link to='/battle'>Battle</Link></li>
+          <li><Link to='/romantic'>Romantic</Link></li>
+          <li><Link to='/school Life'>School Life</Link></li>
+          <li><Link to='/robot'>Robot</Link></li>
+          <li><Link to='/this term'>This term</Link></li>
+          <li><Link to='/my favorite'>My favorite</Link></li>
+        </ul>
+        
+
+      </BrowserRouter>
+    </div>
+  )*/
+}
+
+const Search = () => {
+  return(
+    <div className="search-bar">
+      <input id="search-input" placeholder="please enter the keyword..." 
+      type="text" name="search-key" />
+      
+    </div>
+  )
+}
+
+
 
 export default App;
