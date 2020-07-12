9d01e85f788332adf4a6db803b5d2d664dca3e1f
ntaroooo <45555581+ntaroooo@users.noreply.github.com>
2020-07-12 16:08:54 +0900

    Updates

---
+++ precache-manifest.ac4d63577e1a629ce5058192f2095026.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02cf56e3120ce849d12d4b49107904c4",
+    "url": "/testreact/index.html"
+  },
+  {
+    "revision": "fdd34bdcf077a6585283",
+    "url": "/testreact/static/css/main.d695a422.chunk.css"
+  },
+  {
+    "revision": "a285f3661d8b34da2249",
+    "url": "/testreact/static/js/2.28500325.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/testreact/static/js/2.28500325.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fdd34bdcf077a6585283",
+    "url": "/testreact/static/js/main.d83eabbd.chunk.js"
+  },
+  {
+    "revision": "5cccab1befa68d1ad864",
+    "url": "/testreact/static/js/runtime-main.b1e4199e.js"
+  },
+  {
+    "revision": "5d697f971b56bb0e830f1bf4fbe68d7b",
+    "url": "/testreact/static/media/kimetsu.5d697f97.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/testreact/precache-manifest.0d4bf1b230b1aa68b9bfa42f8d882757.js"
+  "/testreact/precache-manifest.ac4d63577e1a629ce5058192f2095026.js"
 );
 
 self.addEventListener('message', (event) => {
